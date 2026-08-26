6c15ba6267101c286b14f767d8cb8fc3377c53f6
Larobe443 <larobefemme@gmail.com>
2026-08-26 02:50:11 +0200

    Update test.js

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ export const options = {
     ui: {
       executor: 'constant-vus',
       exec: 'browserTest',
-      vus: 3,
+      vus: 13,
       duration: '5h45m',
       options: {
         browser: {
